53325552e3e9656968c61863cdb8d088898dbfa1
xutao <xutao@fruitday.com>
2017-10-10 11:17:05 +0800

    commit

---
+++ src/main/java/javastudy/Basic.java
@@ -0,0 +1,19 @@
+package javastudy;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.text.MessageFormat;
+
+public class Basic {
+    public static void main(String[]args) throws Exception{
+        //MessageFormat
+        System.out.println(  MessageFormat.format("hello {0}", "徐涛")  );
+
+        //获取Resource文件
+        InputStream inputStream = Basic.class.getResourceAsStream("/" + "test.xml");
+        Reader reader = new InputStreamReader(inputStream);
+        System.out.println((char)reader.read());
+    }
+}
